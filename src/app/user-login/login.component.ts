ccb8f4a1e1d8a233ce3e5235f890645fe99ad799
shadesi <45621420+shadesi@users.noreply.github.com>
2024-10-31 00:30:41 +0100

    Update login.component.ts
    updated

--- src/app/user-login/login.component.ts
+++ src/app/user-login/login.component.ts
@@ -1,54 +1,51 @@
-import { Component, OnInit, Input } from '@angular/core';
-
-// You'll use this import to close the dialog on success
+import { Component } from '@angular/core';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatDialogRef } from '@angular/material/dialog';
-
-// This import brings in the API calls we created in 6.2
 import { FetchApiDataService } from '../fetch-api-data.service';
-
-// This import is used to display notifications back to the user
-import { MatSnackBar } from '@angular/material/snack-bar';
-
-import { Router } from '@angular/router';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-user-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'],
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    FormsModule
+  ]
 })
-export class LoginComponent  implements OnInit  {
-
-  @Input() userData = { Username: '', Password: '' };
+export class UserLoginComponent {
+  userData = {
+    Username: '',
+    Password: ''
+  };
 
   constructor(
     public fetchApiData: FetchApiDataService,
-    public dialogRef: MatDialogRef<LoginComponent>,
-    public router: Router,
-    public snackBar: MatSnackBar) { }
-  ngOnInit(): void {
-   
-  }
+    public snackBar: MatSnackBar,
+    public dialogRef: MatDialogRef<UserLoginComponent>
+  ) {}
 
-  /**
-   * Function to login user using FetchApiDataService
-   */
   loginUser(): void {
-    this.fetchApiData.userLogin(this.userData).subscribe((result) => {
-  // Logic for a successful user registration goes here! (To be implemented)
-     localStorage.setItem("user", JSON.stringify(result.user));
-     localStorage.setItem("token", result.token);
-     this.dialogRef.close(); // This will close the modal on success!
-     this.snackBar.open(`Login Successful, Hello ${result.user.Username}`, 'OK', {
-        duration: 2000
-     });
-     this.router.navigate(['movies']);
-
-    }, (result) => {
-      this.snackBar.open("login unsuccessful, please try again", 'OK', {
-        duration: 2000
-      });
-    });
-    this.router.navigate(['welcome']);
+    this.fetchApiData.userLogin(this.userData).subscribe(
+      (result: any) => {
+        localStorage.setItem('user', JSON.stringify(result.user));
+        localStorage.setItem('token', result.token);
+        this.snackBar.open(`Login Successful, Hello ${result.user.Username}`, 'OK', { duration: 2000 });
+        this.dialogRef.close('success');
+      },
+      (error: any) => {
+        const errorMessage = error.error ? error.error.message : 'Login failed';
+        this.snackBar.open(`Login failed: ${errorMessage}`, 'OK', { duration: 2000 });
+      }
+    );
   }
 }
