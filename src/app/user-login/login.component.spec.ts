d8989232cfe3708bf7df2517a79274af358e509e
shadesi <45621420+shadesi@users.noreply.github.com>
2024-10-31 00:30:11 +0100

    Update login.component.spec.ts
    updated

--- src/app/user-login/login.component.spec.ts
+++ src/app/user-login/login.component.spec.ts
@@ -1,19 +1,19 @@
 
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import { UserLoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+  let component: UserLoginComponent;
+  let fixture: ComponentFixture<UserLoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+      declarations: [UserLoginComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(LoginComponent);
+    fixture = TestBed.createComponent(UserLoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
