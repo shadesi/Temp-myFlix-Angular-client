6f3b4b15532094984d6d48f7b5239bbdbeb85d12
shadesi <45621420+shadesi@users.noreply.github.com>
2024-10-31 12:21:25 +0100

    Update fetch-api-data.service.ts
    updated

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -1,5 +1,3 @@
-// src/app/fetch-api-data.service.ts
-
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
@@ -139,92 +137,5 @@ export class FetchApiDataService {
       headers: this.getAuthHeaders(),
       body: { id: userId }
     }).pipe(catchError(this.handleError));
-  }
-}
-
-import { Component } from '@angular/core';
-import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatDialogRef } from '@angular/material/dialog';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { FormsModule } from '@angular/forms';
-
-interface User {
-  Username: string;
-  Password: string;
-  Email: string;
-  Birthday?: Date;
-}
-
-@Component({
-  selector: 'app-user-registration-form',
-  template: `
-    <mat-card>
-      <form (ngSubmit)="registerUser()">
-        <mat-form-field>
-          <input matInput placeholder="Username" [(ngModel)]="userData.Username" name="Username" required>
-        </mat-form-field>
-        <mat-form-field>
-          <input matInput placeholder="Password" [(ngModel)]="userData.Password" name="Password" type="password" required>
-        </mat-form-field>
-        <mat-form-field>
-          <input matInput placeholder="Email" [(ngModel)]="userData.Email" name="Email" type="email" required>
-        </mat-form-field>
-        <mat-form-field>
-          <input matInput placeholder="Birthday" [(ngModel)]="userData.Birthday" name="Birthday" type="date" required>
-        </mat-form-field>
-        <button mat-raised-button color="primary" type="submit">Register</button>
-      </form>
-    </mat-card>
-  `,
-  // styleUrls: ['./user-registration-form.component.scss'],
-  standalone: true,
-  imports: [
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatSnackBarModule,
-    FormsModule
-  ]
-})
-export class UserRegistrationFormComponent {
-  userData: Partial<User> = {
-    Username: '',
-    Password: '',
-    Email: '',
-    Birthday: undefined
-  };
-
-  constructor(
-    public fetchApiData: FetchApiDataService,
-    public snackBar: MatSnackBar,
-    public dialogRef: MatDialogRef<UserRegistrationFormComponent>
-  ) {}
-
-  registerUser(): void {
-    if (!this.userData.Username || !this.userData.Password || !this.userData.Email || !this.userData.Birthday) {
-      this.snackBar.open('Please fill all fields', 'OK', { duration: 2000 });
-      return;
-    }
-
-    // Convert Birthday string to Date object
-    const userDataWithDate: Partial<User> = {
-      ...this.userData,
-      Birthday: new Date(this.userData.Birthday as unknown as string)
-    };
-
-    this.fetchApiData.userRegistration(userDataWithDate).subscribe(
-      (result: any) => {
-        this.dialogRef.close();
-        this.snackBar.open('Registration successful', 'OK', { duration: 2000 });
-      },
-      (error: any) => {
-        const errorMessage = error.error ? error.error.message : 'Registration failed';
-        this.snackBar.open(`Registration failed: ${errorMessage}`, 'OK', { duration: 2000 });
-      }
-    );
   }
 }
